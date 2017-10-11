49deba111192c85eb18da3f0cbff9f43de9ee72a
Luke Bennett <lukepgl@hotmail.com>
2017-10-11 14:42:45 +0100

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+
+var io = socketIO(server);
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+});
+
 console.log(`starting app on port ${port}`);
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`server is up on port ${port}`);
 });
